0ed6e86871839d29e468ca9b09c88bf929a5fce4
crazy_historian <zaitsev808@mail.ru>
2020-10-15 23:00:32 +0300

    Basics of audio processing are added into Audio class.

--- source/model/audio_recording.py
+++ source/model/audio_recording.py
@@ -1,10 +1,30 @@
-class Audio:
+import pyaudio
+import audioop
+
+
+class Audio(pyaudio.PyAudio):
 
     def __init__(self):
+        super().__init__()
+        self.stream = None
+        self.raw_data = None
         self.data = 100
 
     def initStream(self, record_dict):
-        print("Audio:")
-        for characteristic, value in record_dict.items():
-            print(f"{characteristic}: {value}")
-        return self.data
+        self.stream = self.open(
+            format=pyaudio.paInt16,
+            input=True,
+            rate=record_dict['rate'],
+            channels=record_dict['channel'],
+            frames_per_buffer=record_dict['chunk']
+        )
+
+    def readStream(self):
+        self.chunk = self.stream.read()
+
+    def getRMS(self):
+        rms = audioop.rms(self.chunk, 2)
+        return rms
+
+    def closeStream(self):
+        self.stream.close()
